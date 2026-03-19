5923c27d39176d933f2539d4b16a3b462674154a
HELALI <amin.helali@etu.univ-amu.fr>
2026-03-19 16:11:33 +0100

    Refactor App component and add TodoItem component; update animation duration for logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function info() {
+    const info = "les blocs de construction de l’UI";
+    return (
+      <p>Les composants : {info}</p>
+    );
+}
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Welcome to React</h1>
+        <article>
+          <h1>Mon premier composant</h1>
+          {info()}
+            <ol>
+              <li>Les composants : </li>
+              <li>Définir un composant</li>
+              <li>Utiliser un composant</li>
+            </ol>
+        </article>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
