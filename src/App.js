29aadbdc0ce96e44d78da67c051b493b81b24127
HELALI <amin.helali@etu.univ-amu.fr>
2026-03-20 16:56:50 +0100

    Refactor App component: remove unused code, add MyButton component, and delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
-function info() {
-    const info = "les blocs de construction de l’UI";
-    return (
-      <p>Les composants : {info}</p>
-    );
+function MyButton() {
+  return (
+    <button>Je suis un bouton</button>
+  );
 }
-
-
-
-
-
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-        <article>
-          <h1>Mon premier composant</h1>
-          {info()}
-            <ol>
-              <li>Les composants : </li>
-              <li>Définir un composant</li>
-              <li>Utiliser un composant</li>
-            </ol>
-        </article>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ma ToDo-List</h1>
+      <MyButton />
     </div>
   );
 }
